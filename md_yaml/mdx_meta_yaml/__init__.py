3c46395a237fef433cb85317162b9590864cd81b
Jesús Torres <jmtorres@ull.es>
2015-01-29 00:23:45 +0000

    fix #2. Initialization fails with Markdown 2.5.2

--- md_yaml/mdx_meta_yaml/__init__.py
+++ md_yaml/mdx_meta_yaml/__init__.py
@@ -5,4 +5,6 @@ from mdx_meta_yaml.extension import MetaYamlExtension
 def makeExtension(configs=None):
 	if isinstance(configs, list):
 		configs = dict(configs)
+        elif configs is None:
+		configs  = {}
 	return MetaYamlExtension(configs=configs)
